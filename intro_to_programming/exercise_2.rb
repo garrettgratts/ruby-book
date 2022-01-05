cef93380d2ca90bcd25cc35c5842071597a67fd1
Garrett Gratts <garrettgratts21@gmail.com>
2022-01-04 19:29:58 -0500

    update, added spaces after curly bracket

--- intro_to_programming/exercise_2.rb
+++ intro_to_programming/exercise_2.rb
@@ -2,5 +2,5 @@ num = 1234
 
 puts "Thousanth digit is " + "#{ num / 1000 }"
 puts "Hundreth digit is " + "#{ num / 100 % 10 }"
-puts "Tenth digit is " + "#{num / 10 % 10 }"
-puts "Ones digit is " + "#{num / 1 % 10 }"
+puts "Tenth digit is " + "#{ num / 10 % 10 }"
+puts "Ones digit is " + "#{ num / 1 % 10 }"
