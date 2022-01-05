a827bd8bf093f7d61c72501c0571c8f0c48937fa
Garrett Gratts <garrettgratts21@gmail.com>
2022-01-05 15:08:48 -0500

    exercise complete

--- intro_to_programming/exercise_5.rb
+++ intro_to_programming/exercise_5.rb
@@ -7,7 +7,7 @@ puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
 
 =begin
 
-This was my first attempt. Eventually I checked the solution and saw it was asking for simplisic answer.
+This was my first attempt below. Eventually I checked the solution and saw it was asking for simplisic answer.
 
 for num in (10).downto(8)
   puts num
