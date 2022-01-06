c49c15b760b10d52caa8ff5e2e447f0aea903c04
Garrett Gratts <garrettgratts21@gmail.com>
2022-01-06 16:11:59 -0500

    directory name change

---
+++ the_basics/exercise_2.rb
@@ -0,0 +1,6 @@
+num = 1234
+
+puts "Thousanth digit is " + "#{ num / 1000 }"
+puts "Hundreth digit is " + "#{ num / 100 % 10 }"
+puts "Tenth digit is " + "#{ num / 10 % 10 }"
+puts "Ones digit is " + "#{ num / 1 % 10 }"
---
+++ the_basics/exercise_4.rb
@@ -0,0 +1,7 @@
+array = [1999, 1999, 2007, 1998]
+puts array[0]
+puts array[1]
+puts array[2]
+puts array[3]
+
+# Alternate using the each method: array.each { |year| puts year }
