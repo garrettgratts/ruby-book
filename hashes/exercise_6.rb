22fbd4dc9d119e566a76ae065a72c81cc4be4424
Garrett Gratts <garrettgratts21@gmail.com>
2022-01-18 23:57:40 -0600

    complete

---
+++ hashes/exercise_6.rb
@@ -0,0 +1,6 @@
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# The first one has a symbol as the key, while the second one
+# has a string contained within a variable as the key.
---
+++ hashes/exercise_7.rb
@@ -0,0 +1,5 @@
+NoMethodError: undefined method `keys' for Array
+
+B. There is no method called keys for Array objects.
+
+keys is a hash method
