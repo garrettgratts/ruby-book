032b195ed9cfc896f50afe71209e9008bbcba78a
Garrett Gratts <garrettgratts21@gmail.com>
2022-01-23 15:06:02 -0600

    exercise complete

--- exercises/exercise_16.rb
+++ exercises/exercise_16.rb
@@ -1,5 +1,12 @@
-contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
-contacts = {"Joe Smith" => {}}
-info = [email:, address:, phone:]
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"], ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {},"Sally Johnson" => {}}
+fields = [:email, :address, :phone]
 
-contacts["Joe Smith"] = 
+contact_data = contact_data.flatten
+contacts.each do |name, hash|
+  fields.each do |field|
+  hash[field] = contact_data.shift
+  end
+end
+
+p contacts
